4b8bf4e4653e850527127cdd22831693782b1849
Marko Crnkovic <marko777.ny@gmail.com>
2020-06-10 16:40:36 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './img/logo.png';
+import SuperEllipse, { Preset } from 'react-superellipse';
+import { ParallaxHover } from 'react-parallax-hover';
+import { usePalette } from 'react-palette';
 import './App.css';
 
+// TODO: add react helmet
+
 function App() {
+
+  const { data } = usePalette(logo);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div style={{ marginTop: "10vh" }}>
+      <ParallaxHover style={{ backgroundColor: "transparent" }} rotation={5} scale={2} shadow={2} borderRadius={76} width={300} height={300}>
+        <SuperEllipse r1={Preset.iOS.r1} r2={Preset.iOS.r2} style={{ width: "101%", height: "101%" }}>
+          <img src={logo} style={{ width: "100%", height: "100%" }} />
+        </SuperEllipse>
+      </ParallaxHover>
+      <h1 className="title">App Name</h1>
+      <h2>Quick app description</h2>
+      </div>
+      <div className="waitlist">
+        <input className="waitlist" placeholder="Join the Waitlist"></input>
+        <button className="waitlist" style={{ backgroundImage: `linear-gradient(to bottom right, ${data.lightVibrant}, ${data.darkVibrant}` }}>Sign Up</button>
+      </div>
+
     </div>
   );
 }
